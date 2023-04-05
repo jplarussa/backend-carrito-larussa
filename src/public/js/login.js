952e922a47a2079832d38c5fe48f03a127efe9ab
Jean Pierre La Russa <jplarussa@gmail.com>
2023-04-04 23:58:30 -0300

    Fix bug pagina products

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -17,7 +17,7 @@ form.addEventListener('submit', e => {
         }
     }).then(result => {
         if (result.status === 200) {
-            window.location.replace('/users');
+            window.location.replace('/products');
 
         } else if (result.status === 401) {
             console.log(result);
