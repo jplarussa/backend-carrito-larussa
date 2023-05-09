f7d224590ca5c37f3f6b29af3e2af19705973b16
Jean Pierre La Russa <jplarussa@gmail.com>
2023-05-09 04:03:34 -0300

    Ajustes en controllers y routers

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
-import { register, login, getCurrent, logout, restorePass, gitHubLogin, gitHubCallback } from '../controllers/auth.controller.js';
+import { register, login, gitHubLogin, gitHubCallback, getCurrent, logout, restorePass } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/github", gitHubLogin);
+router.get("/githubcallback", gitHubCallback);
+
 router.get("/current", getCurrent)  
 router.post("/logout", logout);
 router.post("/restore", restorePass);
-router.get("/github", gitHubLogin);
-router.get("/githubcallback", gitHubCallback)  
+
+
 
 export default router;
