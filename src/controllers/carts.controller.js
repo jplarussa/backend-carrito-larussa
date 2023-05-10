309e1482ecd584f8d6a2e84d28727faad843686a
Jean Pierre La Russa <jplarussa@gmail.com>
2023-05-09 23:06:15 -0300

    Edit carts.controller

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,11 +1,16 @@
+import CartManager from "../dao/db/carts.dao.js";
+
+const cartManager = new CartManager();
 
 export const addCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Cart created successfully!", payload: "addCart"});
+        const cartCreated = await cartManager.addCart();
+        res.status(201).send(cartCreated);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error creating the cart."});
     }
     
 }
@@ -13,60 +18,94 @@ export const addCart = async (req, res) => {
 export const addProductToCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Success! cartUpdate.message/result", payload: "addProductToCart"});
+        const cartId = req.params.cid;
+        const productId = req.params.pid;
+
+        const cartUpdate = await cartManager.addProductToCart(cartId, productId);
+
+        res.status(201).send(cartUpdate);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error adding the product to the cart"});
     }
     
 }
 export const getCart = async (req, res) => {
     try {
 
-        res.status(200).send({message: "Success! cartUpdate.message", payload: "getCart"});
+        const cartId = req.params.cid;
+        const result = await cartManager.getCart(cartId);
+
+        res.status(201).send(result);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error error searching the cart."});
     }
     
 }
 export const deleteProductFromCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Success! cartUpdate.message", payload: "deleteProductFromCart"});
+        const cartId = req.params.cid;
+        const productId = req.params.pid;
+
+        const cartUpdate = await cartManager.deleteProductFromCart(cartId, productId);
+
+        res.status(201).send(cartUpdate);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error deleting the product in the cart"});
     }
     
 }
 export const updateCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Success! cartUpdate.message", payload: "updateCart"});
+        const cartId = req.params.cid;
+        const newProducts = req.body.products;
+
+        const cartUpdate = await cartManager.updateCart(cartId, newProducts);
+
+        res.status(201).send(cartUpdate);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error updating products in the cart"});
     }
     
 }
 export const updateProductQuantityInCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Success! cartUpdate.message", payload: "updateProductQuantityInCart"});
+        const cartId = req.params.cid;
+        const productId = req.params.pid;
+        const quantity = req.body.quantity;
+
+        const cartUpdate = await cartManager.updateProductQuantityInCart(cartId, productId, quantity);
+
+        res.status(201).send(cartUpdate);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error updating quantity in the cart"});
     }
     
 }
 export const emptyCart = async (req, res) => {
     try {
 
-        res.status(201).send({message: "Success! cartUpdate.message", payload: "emptyCart"});
+        const cartId = req.params.cid;
+
+        const cartUpdate = await cartManager.emptyCart(cartId);
+
+        res.status(201).send(cartUpdate);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: error, message: "Product could not be loaded"});
+        res.status(500).send({error: error, message: "Error emptying the cart"});
     }
     
 }
@@ -76,7 +115,7 @@ export const emptyCart = async (req, res) => {
 ********************************************************************************************************************
 ********************************************************************************************************************
 ********************************************************************************************************************
-
+ 
 
 //import of the service for Carts. (You can change to file system by swapping the commented line)
 // import CartManager from "../dao/fs/CartManager.js";
@@ -223,4 +262,4 @@ router.delete('/:cid', async (request, response) => {
     }
 
 });
- */
+*/
