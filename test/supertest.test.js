6d2da0010e1920023e20017ecf0dad79675c8e59
Jean Pierre La Russa <jplarussa@gmail.com>
2023-06-21 00:35:44 -0300

    Agrego current en session test

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -27,13 +27,13 @@ describe("Integration test - Ecommerce App", () => {
             mongoose.connection.collections.users.drop();
         }); */
 
-        it("Resgister user", async function(){
+        it("Register user - POST /api/sessions/register", async function(){
             const {statusCode, ok, _body} = await requester.post("/api/sessions/register").send(this.mockUser);
             expect(statusCode).to.be.eql(201)
             expect(_body.message).to.be.deep.equal('Success')
         });
 
-        it("Login user and set cookie", async function(){
+        it("Login user and set cookie- POST /api/sessions/login", async function(){
 
             const result = await requester.post("/api/sessions/login").send({ email: this.mockUser.email, password: this.mockUser.password});
             const cookieResult = result.headers['set-cookie'][0];
@@ -50,6 +50,13 @@ describe("Integration test - Ecommerce App", () => {
             expect(this.cookie.name).to.be.ok.and.eql('jwtCookieToken');
             expect(this.cookie.value).to.be.ok
         });
+
+        it("Endpoint Current have a user with cookie - GET /api/sessions/current", async function() {
+            const{ _body, } = await requester.get("/api/sessions/current").set("Cookie", [`${this.cookie.name}=${this.cookie.value}`])
+            console.log( _body);
+            expect(_body.email).to.be.ok.and.eql("someone@gmail.com")
+
+        })
 
     });
 
